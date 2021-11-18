3fa1297b6feb6b2de17ba5268709c2d18cf864a4
韩方泽 <934913780@qq.com>
2021-11-18 17:14:42 +0800

    map

--- .vscode/map/03.cpp
+++ .vscode/map/03.cpp
@@ -1,2 +1,94 @@
-#include<iostream>
+#include <iostream>
+#include <limits.h>
 using namespace std;
+#define MaxInt INT_MAX
+#define MaxN 20
+typedef struct
+{
+    char vexs[MaxN];
+    int arcs[MaxN][MaxN];
+    int vexnum, arcnum;
+} MGraph;
+int i, j;
+bool visited[MaxN];
+int Locate(MGraph G, char v)
+{
+    for (j = 0; j < G.vexnum; j++)
+    {
+        if (G.vexs[j] == v)
+        {
+            return j;
+        }
+    }
+    return -1;
+}
+void CreatUDN(MGraph &G)//创建邻接矩阵
+{
+    int a, b, w;
+    char v1, v2;
+    cin >> G.vexnum >> G.arcnum;
+    for (i = 0; i < G.vexnum; i++)
+        cin >> G.vexs[i];
+    for (i = 0; i < G.vexnum; i++)
+    {
+        for (j = 0; j < G.vexnum; j++)
+            G.arcs[i][j] = MaxInt;
+    }
+    for (i = 0; i < G.arcnum; i++)
+    {
+        cin >> v1 >> v2 >> w;
+        a = Locate(G, v1);
+        b = Locate(G, v2);
+        G.arcs[a][b] = w;
+        G.arcs[b][a] = G.arcs[a][b];
+    }
+}
+int FirstAdjVex(MGraph G,int v)//搜索第一个邻接点
+{
+    for (j = 0; j < G.vexnum;j++)
+    {
+        if(G.arcs[v][j]!=MaxInt)
+            return j;
+    }
+    return -1;
+}
+int NextAdjVex(MGraph G,int v)//搜索下一个邻接点
+{
+    for (j=j+1; j < G.vexnum;j++)
+    {
+        if(G.arcs[v][j]!=MaxInt)
+            return j;
+    }
+    return -1;
+}
+void DFS(MGraph G,int v)
+{
+    int w;
+    cout << G.vexs[v] << " ";
+    visited[v] = true;
+    for (w = FirstAdjVex(G, v); w >= 0;w=NextAdjVex(G,v))
+    {
+        if(!visited[w])
+            DFS(G, w);
+    }
+}
+void DFSTraverse(MGraph G)//深度优先搜索
+{
+    for (i = 0; i < G.vexnum;i++)
+    {
+        visited[i] = false;
+    }
+    for (i = 0; i < G.vexnum;i++)
+    {
+        if(!visited[i])
+            DFS(G, i);
+    }
+}
+int main()
+{
+    MGraph G;
+    CreatUDN(G);
+    DFSTraverse(G);
+    system("pause");
+    return 0;
+}
--- .vscode/map/04.cpp
+++ .vscode/map/04.cpp
@@ -1,2 +1,101 @@
-#include<iostream>
+#include <iostream>
+#include<queue>
 using namespace std;
+typedef struct ArcNode
+{
+    int adjvex;
+    struct ArcNode *nextarc;
+    int info;
+} ArcNode;
+typedef struct VNode
+{
+    char data;
+    ArcNode *firstarc;
+} VNode, AdjList[20];
+typedef struct
+{
+    AdjList vertices;
+    int vexnum;
+    int arcnum;
+} ALGraph;
+int i, j;
+bool visited[20];
+int LocateVex(ALGraph G, char v)
+{
+    for (j = 0; j < G.vexnum; j++)
+    {
+        if (G.vertices[j].data == v)
+            return j;
+    }
+    return -1;
+}
+void CreatGraph(ALGraph &G) //创建邻接表
+{
+    int a, b, w;
+    char v1, v2;
+    cin >> G.vexnum >> G.arcnum;
+    for (i = 0; i < G.vexnum; i++)
+    {
+        cin >> G.vertices[i].data;
+        G.vertices[i].firstarc = NULL;
+    }
+    for (i = 0; i < G.arcnum; i++)
+    {
+        cin >> v1 >> v2 >> w;
+        a = LocateVex(G, v1);
+        b = LocateVex(G, v2);
+        ArcNode *p1 = (ArcNode *)malloc(sizeof(ArcNode));
+        p1->adjvex = b;
+        p1->info = w;
+        p1->nextarc = G.vertices[a].firstarc;
+        G.vertices[a].firstarc = p1;
+        ArcNode *p2 = (ArcNode *)malloc(sizeof(ArcNode));
+        p2->adjvex = a;
+        p2->info = w;
+        p2->nextarc = G.vertices[b].firstarc;
+        G.vertices[b].firstarc = p2;
+    }
+}
+void BFSTraverse(ALGraph G)//广度优先搜索
+{
+    int u, w, v;
+    for (v = 0; v < G.vexnum;v++)
+    {
+        visited[v] = false;
+    }
+    queue<int> Q;
+    for (v = 0; v < G.vexnum;v++)
+    {
+        if(!visited[v])
+        {
+            visited[v] = true;
+            cout << G.vertices[v].data << " ";
+            Q.push(v);
+            while(!Q.empty())
+            {
+                u = Q.front();
+                Q.pop();
+                ArcNode *p = G.vertices[u].firstarc;
+                while(p)
+                {
+                    if(!visited[p->adjvex])
+                    {
+                        cout << G.vertices[p->adjvex].data << " ";
+                        visited[p->adjvex] = true;
+                        Q.push(p->adjvex);
+                    }
+                    p = p->nextarc;
+                }
+            }
+            
+        }
+    }
+}
+int main()
+{
+    ALGraph G;
+    CreatGraph(G);
+    BFSTraverse(G);
+    system("pause");
+    return 0;
+}
